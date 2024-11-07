9588b4e6c57f199476884916bd7bc5d40ef89ca9
mjm836 <mjm836>
2024-11-07 13:13:21 -0500

    removed unused dependency

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:itegration_test_form_example/home_screen.dart';
 import 'package:itegration_test_form_example/login_screen.dart';
 import 'package:itegration_test_form_example/main.dart' as app;
